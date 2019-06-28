cb2b1f9a24c1299e4e4de47eda1c34fc44676326
Grace Lee <igracelee1@gmail.com>
2019-06-28 16:48:57 -0400

    styling for SongList

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class SongList extends Component {
+
+    renderList() {
+        return this.props.songs.map((song => {
+            return (
+                <div className="item" key={song.title}>
+                    <div className="right floated content" >
+                        <button className="ui primary button">
+                            Select
+                        </button>
+                    </div>
+                <div className="content">
+                    {song.title}
+                </div>
+                </div>
+            );
+        }));
+    };
+
     render() {
         return (
-            <div>
-                SongList
+            <div className="ui divided list">
+                {this.renderList()}
             </div>
         )
     }
@@ -19,7 +37,7 @@ class SongList extends Component {
 */
 
 const mapStateToProps = (state) => {
-    return { songs: state.songs };
+    return { songs: state.songs};
 };
 
 export default connect(mapStateToProps)(SongList);
